6770281735d7a294eddcdebd2011894e21f0db6e
Konrad Krenzlin <git@krenzlin.com>
2019-06-28 18:32:31 +0200

    removed all generator stuff writing sample by sample, still strange output

--- main.ino
+++ main.ino
@@ -12,63 +12,51 @@
 #define DAC_MAX_VALUE 255.0 // IMPORTANT THAT IT'S A FLOAT, OTHERWISE ALL MATH IS CASTED TO INT
 
 
-static const i2s_config_t i2s_config = {
-    .mode = I2S_USE_BUILT_IN_DAC,
-    .sample_rate = SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, /* the DAC module will only take the 8bits from MSB */
-    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-    .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_I2S_MSB,
-    .intr_alloc_flags = 0, // default interrupt priority
-    .dma_buf_count = 2,  // ?? number of linked DMA buffers
-    // according to https://www.esp32.com/viewtopic.php?t=10140#p41782
-    // DMA uses multiple buffers to be filled (one to be read) and round-robins between them.
-    .dma_buf_len = BUFFER_SIZE, // length of each DMA buffer (in samples)
-    //.tx_desc_auto_clear = true,
-    .use_apll = false
-};
+void setup_I2S() {
+    i2s_config_t i2s_config = {
+        .mode = I2S_USE_BUILT_IN_DAC,
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, /* the DAC module will only take the 8bits from MSB */
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_I2S_MSB,
+        .intr_alloc_flags = 0, // default interrupt priority
+        .dma_buf_count = 2,  // ?? number of linked DMA buffers
+        // according to https://www.esp32.com/viewtopic.php?t=10140#p41782
+        // DMA uses multiple buffers to be filled (one to be read) and round-robins between them.
+        .dma_buf_len = BUFFER_SIZE, // length of each DMA buffer (in samples)
+        //.tx_desc_auto_clear = true,
+        .use_apll = false
+    };
 
+    esp_err_t err = i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    Serial.printf("I2S install: %d\nr\r", err);
 
-class Sawtooth {
-    public:
-        Sawtooth(float sample_rate): sr{sample_rate} {};
-        void SetFrequency(float frequency);
-        void compute(unsigned int buffer[], size_t len);
-        float next_sample();
-    private:
-        float sr;
-        float frequency;
-        float phase {0.0};
-        float phase_increment;
-};
-
-
-static Sawtooth saw{SAMPLE_RATE};
-
+    // important otherwise no sound
+    i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
+    i2s_set_sample_rates(I2S_NUM_0, SAMPLE_RATE);
+    i2s_zero_dma_buffer(I2S_NUM_0);
+}
 
 void setup() {
     Serial.begin(115200);
 
-    esp_err_t err = i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-    Serial.printf("installed: %d\nr\r", err);
-
-    // important otherwise no sound
-    i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
-
-    i2s_set_sample_rates(I2S_NUM_0, SAMPLE_RATE);
-
-    i2s_zero_dma_buffer(I2S_NUM_0);
-
-    saw.SetFrequency(440.0);
+    setup_I2S();
 }
 
 void loop() {
-    static unsigned int buffer[BUFFER_SIZE];
+    static float frequency {440.0};
+    static float phase {0.0};
+    static float phase_increment {float(frequency) / float(SAMPLE_RATE)};
 
-    saw.compute(buffer, BUFFER_SIZE);
-    Serial.printf("buffer[10]: %u\n\r", buffer[10]);
+    unsigned int sample = (unsigned int)(phase * DAC_MAX_VALUE);
+
+    phase += phase_increment;
+    if (phase > 1.0) {
+        phase -= 1.0;
+    }
 
     static size_t bytes_written = 0;
-    esp_err_t err = i2s_write(I2S_NUM_0, buffer, BUFFER_SIZE, &bytes_written, portMAX_DELAY);
+    esp_err_t err = i2s_write(I2S_NUM_0, &sample, 1, &bytes_written, portMAX_DELAY);
     Serial.printf("err: %d, bytes written: %d\n\r", err, bytes_written);
 
     /*
@@ -78,26 +66,4 @@ void loop() {
     blocking behaviour will make sure the timing is OK."
     from: https://www.esp32.com/viewtopic.php?t=3168#p14931
     */
-}
-
-
-void Sawtooth::SetFrequency(float frequency) {
-    Sawtooth::frequency = frequency;
-    Sawtooth::phase_increment = Sawtooth::frequency / Sawtooth::sr;
-}
-
-float Sawtooth::next_sample() {
-    phase += phase_increment;
-    while (phase > 1.0) {
-        phase -= 1.0;
-    }
-
-    return phase;
-}
-
-void Sawtooth::compute(unsigned int buffer[], size_t len) {
-    for (auto i=0; i<len; i++) {
-        float sample = Sawtooth::next_sample()*DAC_MAX_VALUE;
-        buffer[i] = (unsigned int)sample;
-    }
 }
