a30a0568a51ccd77a5b1d4ad4cba01e887f1d760
Konrad Krenzlin <git@krenzlin.com>
2019-07-09 14:10:17 +0200

    8bit simulation and note on/off in write_to_file

--- write_to_file.cpp
+++ write_to_file.cpp
@@ -2,23 +2,40 @@
 
 #include <iostream>
 #include <stdio.h>
+#include "config.h"
 #include "generators.h"
+
+float simulate_8bit(float value) {
+    return int(value * 128) / 128.0;
+}
 
 
 int main() {
-    SmoothSin osc;
-    osc.set_frequency(440.0);
+    Saw osc;
+    osc.on(440.0);
+    osc.set_ADSR(0.5*SAMPLE_RATE, 0.2*SAMPLE_RATE, 0.3, 0.5*SAMPLE_RATE);
 
-    float buffer[SAMPLE_RATE];
+    float sample {0.0};
+    int len {int(2*SAMPLE_RATE)};
+    float buffer[len];
     
+    std::FILE* file = std::fopen("generated.raw", "wb");
+
+    for (auto i=0; i<SAMPLE_RATE; i++) {
+        sample = osc.next_sample();
+        buffer[i] = simulate_8bit(sample);
+    }
+    std::fwrite(buffer, sizeof(float), SAMPLE_RATE, file);
+
+    osc.off();
     for (auto i=0; i<SAMPLE_RATE; i++) {
         buffer[i] = osc.next_sample();
     }
-    std::cout << "sample rate "  << SAMPLE_RATE << std::endl;
 
-    std::FILE* file = std::fopen("generated.raw", "wb");
+
     std::fwrite(buffer, sizeof(float), SAMPLE_RATE, file);
     std::fclose(file);
+    std::cout << "sample rate "  << SAMPLE_RATE << std::endl;
 
     return 0;
 }
