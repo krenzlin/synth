be7911ad0a7e81959ec80b0474fa61258a493b48
Konrad Krenzlin <git@krenzlin.com>
2019-08-02 16:32:50 +0200

    Play random chords in Arduino sketch

--- main.ino
+++ main.ino
@@ -5,15 +5,19 @@
 #include "config.h"
 #include "defs.h"
 #include "sound.h"
+#include "generators.h"
 
 // declare Synth/Audio object in global namespace to keep it alive for the audio thread
-VoiceManager<WavetableSine> vm;
+VoiceManager<FM> vm {};
+VoiceParams tone {};
+VoiceParams kick {};
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+//MIDI_CREATE_DEFAULT_INSTANCE();
 
 void handleNoteOn(byte channel, byte pitch, byte velocity) {
     digitalWrite(LED, HIGH);
-    vm.note_on(int(pitch), int(velocity));
+    vm.note_on(int(pitch), int(velocity), tone);
+    delay(10);
     digitalWrite(LED, LOW);
 }
 
@@ -37,23 +41,33 @@ void setup() {
     create_wavetable();
     blink(1);
 
-    MIDI.setHandleNoteOn(handleNoteOn);
-    MIDI.setHandleNoteOff(handleNoteOff);
-    MIDI.begin(MIDI_CHANNEL_OMNI);
+    //MIDI.setHandleNoteOn(handleNoteOn);
+    //MIDI.setHandleNoteOff(handleNoteOff);
+    //MIDI.begin(MIDI_CHANNEL_OMNI);
 
     vm.init();
+    vm.set_ADSR(0.5, 0.0, 1.0, 1.0);
 
     audio(vm);
 }
 
 void loop() {
-    MIDI.read();
-    for (auto i=0; i<10; i++) {
-        float decay = float(i) / 20.0;
-        vm.set_ADSR(0.01, decay, 0.0, 0.0);
-        vm.note_on(40, 127);
-        delay(500);
-        vm.note_off(40, 127);
-        delay(100);
-    }
+    //MIDI.read();
+    int note1 = random_MIDI_note(60, 72);
+    int note2 = random_MIDI_note(60, 72);
+    int note3 = random_MIDI_note(60, 72);
+
+
+    vm.note_on(note1, 127, tone);
+    delay(250);
+    vm.note_on(note2, 127, tone);
+    delay(250);
+    vm.note_on(note3, 127, tone);
+    delay(250);
+    vm.note_off(note1, 127);
+    delay(50);
+    vm.note_off(note2, 127);
+    delay(50);
+    vm.note_off(note3, 127);
+    delay(50);
 }
