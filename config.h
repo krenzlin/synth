779a92b3b4efe2ac2823d1221faf09b7afb89ee4
Konrad Krenzlin <git@krenzlin.com>
2019-08-21 14:29:31 +0200

    Raise max voices after using -O3 optimization when compiling

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ namespace config {
     const float SAMPLE_RATE {44100.0};  // FLOAT !!!
     const int BUFFER_SIZE {32};
 
-    const int MAX_VOICES {6};
+    const int MAX_VOICES {10};
     const float TUNING {440.0};
 
     const int WAVETABLE_SIZE {2048};
