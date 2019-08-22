66f7a2e9ff9b0530d4bfc0692e190f690f8cae7d
Konrad Krenzlin <git@krenzlin.com>
2019-08-22 23:26:12 +0200

    Refine AudioBase to Generator struct and add Voice and VoiceManager

--- osc.hpp
+++ osc.hpp
@@ -1,36 +1,120 @@
 #pragma once
 
+#include "ADSR.h"
 #include "config.h"
-#include "generators.h"
 #include "helper.h"
 
-class AudioBase {
-    virtual void process(float buffer[], int buffer_size=config::BUFFER_SIZE);
-    virtual float dsp() = 0;
-    virtual void calculate_parameters() {};
-    virtual void init(float frequency, int velocity) {}
+struct Generator {
+    virtual double sample() = 0;
 };
 
 
-void AudioBase::process(float buffer[], int buffer_size) {
-    calculate_parameters();
-    for (auto i=0; i < buffer_size; i++) {
-        buffer[i] = dsp();
-    }
-}
+struct Saw : Generator {
+    double frequency {0.0};
+    double phase {0.0};
 
+    double sample() {
+        double p_incr = phase_increment(frequency);
+        double sample =  phase - poly_blep(phase, p_incr);
+        sample = zero_one_to_minus_plus(sample);
 
-class SimpleSine : AudioBase {
-    float phase {0.0};
-    float phase_incr {0.0};
-
-    void init(float frequency, int velocity) {
-        phase_incr = phase_increment(frequency);
-    }
-
-    float dsp() {
-        float sample = fast_sine(phase);
-        phase += phase_incr;
+        phase += p_incr;
         phase = mod_phase(phase);
+
+        return sample;
+    }
+};
+
+struct Sine : Generator {
+    double frequency {0.0};
+    double phase {0.0};
+
+    double sample() {
+        double sample = fast_sine(phase);
+
+        phase += phase_increment(frequency);
+        phase = mod_phase(phase);
+        return sample;
+    }
+};
+
+struct Noise : Generator {
+    double sample() {
+        return fast_rand_float();
+    }
+};
+
+
+template<typename T>
+struct Voice : Generator {
+    T osc {};
+    ADSR env {};
+    bool active {false};
+
+    bool is_active() {
+        return env.getState();
+    }
+
+    double sample() {
+        if (!is_active()) {
+            return 0.0;
+        }
+
+        return double(osc.sample() * env.process());
+    }
+
+    void note_on(int pitch, int velocity) {
+        double frequency = mtof(pitch);
+        osc.frequency = frequency;
+
+        env.reset();
+        env.gate(true);
+    }
+
+    void note_off(int velocity) {
+        env.gate(false);
+    }
+};
+
+
+template<typename T>
+struct VoiceManager : Generator {
+    Voice<T> voices[config::MAX_VOICES];
+    Voice<T>* notes[127];
+
+    VoiceManager() {
+        for (auto &voice : voices) {
+            voice = Voice<T> {};
+        }
+    }
+
+    double sample() {
+        double sample = 0.0;
+
+        for (auto &voice : voices) {
+            if (voice.is_active()) {
+                sample += voice.sample();
+            }
+        }
+        sample /= double(config::MAX_VOICES);
+
+        return sample;
+    }
+
+    void note_on(int pitch, int velocity) {
+        for (auto &voice : voices) {
+            if (!voice.is_active()) {
+                voice.note_on(pitch, velocity);
+                notes[pitch] = &voice;
+                break;
+            }
+        }
+    }
+
+    void note_off(int pitch, int velocity) {
+        if (notes[pitch]) {
+            notes[pitch]->note_off(velocity);
+            notes[pitch] = nullptr;
+        }
     }
 };
