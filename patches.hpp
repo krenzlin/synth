2fc8442c212ba45f6ee65e31694c49db5f8f3595
Konrad Krenzlin <git@krenzlin.com>
2019-08-26 22:03:44 +0200

    Organ: Add bit of distortion and change frequenies of harmonics

--- patches.hpp
+++ patches.hpp
@@ -1,17 +1,20 @@
 #include "osc.hpp"
 
+inline float dist(float x, float a=0.0) {
+    return (1.0 + a)*x / (1.0 + a*x*x);
+}
 
 struct Organ : Generator {
     Double<Sine, DriftingSine> h0 {};
     Double<Sine, DriftingSine> h1 {};
     Double<Sine, DriftingSine> h2 {};
+    float dist_amount = 0.28;
 
     void note_on(float frequency, float velocity) {
 
         h0.note_on(frequency, velocity);
-        h1.note_on(frequency*1.5, velocity*0.707);
-        h2.note_on(frequency*2.0, velocity*0.707*0.707);
-
+        h1.note_on(frequency*2.0, velocity*0.707);
+        h2.note_on(frequency*4.0, velocity*0.707*0.707);
 
         h0.mix = 0.707;
         h1.mix = 0.707;
@@ -19,8 +22,8 @@ struct Organ : Generator {
     }
 
     float sample() {
-        return h0.sample() + \
+        return dist(h0.sample() + \
                 h1.sample() + \
-                h2.sample();
+                h2.sample(), dist_amount);
     }
 };
