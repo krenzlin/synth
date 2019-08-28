40e53e78c42f4e6fe5c7ecda5dc677e449ec6d45
Konrad Krenzlin <git@krenzlin.com>
2019-08-28 13:03:44 +0200

    Update patches.hpp for new interfaces

--- patches.hpp
+++ patches.hpp
@@ -1,27 +1,27 @@
 #include "osc.hpp"
+#include "voice.hpp"
 
 inline float dist(float x, float a=0.0) {
     return (1.0 + a)*x / (1.0 + a*x*x);
 }
 
-struct Organ : Generator {
+struct Organ : Oscillator {
     Double<Sine, DriftingSine> h0 {};
     Double<Sine, DriftingSine> h1 {};
     Double<Sine, DriftingSine> h2 {};
     float dist_amount = 0.28;
 
-    void note_on(float frequency, float velocity) {
-
-        h0.note_on(frequency, velocity);
-        h1.note_on(frequency*2.0, velocity*0.707);
-        h2.note_on(frequency*4.0, velocity*0.707*0.707);
+    void on(float frequency, float velocity) override {
+        h0.on(frequency, velocity);
+        h1.on(frequency*2.0, velocity*0.707);
+        h2.on(frequency*4.0, velocity*0.707*0.707);
 
         h0.mix = 0.707;
         h1.mix = 0.707;
         h2.mix = 0.707;
     }
 
-    float sample() {
+    float sample() override {
         return dist(h0.sample() + \
                 h1.sample() + \
                 h2.sample(), dist_amount);
@@ -29,20 +29,20 @@ struct Organ : Generator {
 };
 
 
-struct StringsOsc : Generator {
+struct StringsOsc : Oscillator {
     Double<Saw, DriftingSaw> osc {};
     Lowpass lp;
 
-    void note_on(float frequency, float velocity) {
+    void on(float frequency, float velocity) override {
         lp.set_cutoff(frequency);
         osc.mix = 0.8;
         osc.osc1.phase = random_phase();
         osc.osc2.phase = random_phase();
         osc.osc2.max_steps = 20;
-        osc.note_on(frequency, velocity);
+        osc.on(frequency, velocity);
     }
 
-    float sample() {
+    float sample() override {
         return lp.process(osc.sample());
     }
 };
